021995cdcf7d3e789bc64ceb0a93fff06921e562
RegalEagles2869 <theregaleagles2869@gmail.com>
2024-01-24 15:27:42 -0500

    Ramping Code

--- src/main/java/frc/robot/Inputs.java
+++ src/main/java/frc/robot/Inputs.java
@@ -3,6 +3,7 @@ package frc.robot;
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -26,30 +27,35 @@ public class Inputs {
 
         return total/list.size();
     }
-
+    private static SlewRateLimiter translationXSlew = new SlewRateLimiter(10, -2, 0);
     
     private static List<Double> translationXList = new ArrayList<>(10); 
     public static double getTranslationX(){
 		// return 0.0;
-		double speed = -driver1.getLeftX();
+		double speed = driver1.getLeftX();
 		if(Math.abs(speed) < .1){
 			speed = 0;
 		}
-
-        speed = smoothInputs(speed, translationXList);
-
+        speed = translationXSlew.calculate(speed);
+        // speed = smoothInputs(speed, translationXList);
+        SmartDashboard.putNumber("X", speed);
         return speed;
     }
+
+
+    private static SlewRateLimiter translationYSlew = new SlewRateLimiter(10, -2, 0);
 
     private static List<Double> translationYList = new ArrayList<>(10); 
     public static double getTranslationY(){
 		// return 0.0;
-		double speed = -driver1.getLeftY();
+		double speed = driver1.getLeftY();
 		if(Math.abs(speed) < .1){
 			speed = 0;
 		}
 
-        speed = smoothInputs(speed, translationYList);
+        speed = translationYSlew.calculate(speed);
+        // speed = smoothInputs(speed, translationYList);
+        SmartDashboard.putNumber("Y", speed);
 
         return speed;
     }
@@ -57,12 +63,13 @@ public class Inputs {
     private static List<Double> rotationList = new ArrayList<>(10); 
     public static double getRotation(){
 		// return 0.0;
-		double speed = -driver1.getRightX();
+		double speed = driver1.getRightX();
 		if(Math.abs(speed) < .1){
 			speed = 0;
 		}
 
-        speed = smoothInputs(speed, rotationList);
+        // speed = smoothInputs(speed, rotationList);
+        SmartDashboard.putNumber("Z", speed);
 
         return speed;
     }
